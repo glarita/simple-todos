47ea52c212e22db2d4cfeda12ee120b6bc732d2f
Glen Larita <glarita@hawaii.edu>
2020-10-18 00:32:25 -1000

    Section 2

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
 
-const tasks = [
-  { _id: 1, text: 'Ichi Task' },
-  { _id: 2, text: 'Ni Task' },
-  { _id: 3, text: 'San Task' },
-];
 
-export const App = () => (
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  return (
     <div>
       <h1>Welcome to Meteor!</h1>
       <ul>
-        { tasks.map(task => <Task key={task._id} task={task}/>)}
+        {tasks.map(task => <Task key={task._id} task={task}/>)}
       </ul>
     </div>
-);
+ );
+};
