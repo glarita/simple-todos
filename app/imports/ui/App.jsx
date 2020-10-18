9b54a2ef1294a0e1184c65738cb7a3241aaa096c
Glen Larita <glarita@hawaii.edu>
2020-10-18 11:04:17 -1000

    Section 4

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -4,17 +4,32 @@ import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
 import { TaskForm } from './TaskForm';
 
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
+
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}, { sort: {createdAt: -1 } }).fetch());
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
   return (
-    <div>
-      <h1>Welcome to Meteor!</h1>
+      <div>
+        <h1>Welcome to Meteor!</h1>
 
-      <TaskForm/>
+        <TaskForm/>
 
-      <ul>
-        {tasks.map(task => <Task key={task._id} task={task}/>)}
-      </ul>
-    </div>
- );
+        <ul>
+          {tasks.map(task => <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={toggleChecked}
+              onDeleteClick={deleteTask}
+          />) }
+        </ul>
+      </div>
+  );
 };
