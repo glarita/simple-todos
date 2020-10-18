a0549ee63abc4a32d67e55b712c2cfd44837a939
Glen Larita <glarita@hawaii.edu>
2020-10-17 23:34:15 -1000

    Section 1B

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { Task } from './Task';
+
+const tasks = [
+  { _id: 1, text: 'Ichi Task' },
+  { _id: 2, text: 'Ni Task' },
+  { _id: 3, text: 'San Task' },
+];
 
 export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
+    <div>
+      <h1>Welcome to Meteor!</h1>
+      <ul>
+        { tasks.map(task => <Task key={task._id} task={task}/>)}
+      </ul>
+    </div>
 );
